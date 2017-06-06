720ea8d763144eb6bcba27998c5124b92126aa73
Usua Madina <u.madina.i@gmail.com>
2017-06-06 20:58:49 +0200

    Creating a selection list using WearebleRecyclerView

--- wear/src/main/java/example/com/paddelwear/MainActivity.java
+++ wear/src/main/java/example/com/paddelwear/MainActivity.java
@@ -1,17 +1,31 @@
 package example.com.paddelwear;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.wearable.view.WatchViewStub;
-import android.widget.TextView;
+import android.support.wearable.view.WearableRecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+
 
 public class MainActivity extends Activity {
+// Elementos a mostrar en la lista
 
-    private TextView mTextView;
+    String[] elements = {"Partida", "Terminar partida", "Historial", "Notificación", "Pasos", "Pulsaciones", "Terminar partida"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WearableRecyclerView lista = (WearableRecyclerView) findViewById(R.id.list);
+        MyAdapter myAdapter = new MyAdapter(this, elements);
+        myAdapter.setOnItemClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer tag = (Integer) v.getTag();
+                Toast.makeText(MainActivity.this, "Elegida opción:" + tag, Toast.LENGTH_SHORT).show();
+            }
+        });
+        lista.setAdapter(myAdapter);
     }
+
 }
---
+++ wear/src/main/java/example/com/paddelwear/MyAdapter.java
@@ -0,0 +1,68 @@
+package example.com.paddelwear;
+
+/**
+ * Created by usuwi on 06/06/2017.
+ */
+
+import android.content.Context;
+import android.support.wearable.view.WearableRecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by usuwi on 06/06/2017.
+ */
+
+final class MyAdapter extends WearableRecyclerView.Adapter {
+    private String[] datos;
+    private final Context contexto;
+    private final LayoutInflater inflador;
+    protected View.OnClickListener onClickListener;
+
+    public MyAdapter(Context contexto, String[] datos) {
+        this.contexto = contexto;
+        this.datos = datos;
+        inflador = LayoutInflater.from(contexto);
+    } //Creamos nuestro ViewHolder, con los tipos de elementos a modificar
+
+    public static class ItemViewHolder extends WearableRecyclerView.ViewHolder {
+        private TextView textView;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.name);
+        }
+    } // Creamos el ViewHolder con las vista de un elemento sin personalizar
+
+    @Override
+    public WearableRecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Inflamos la vista desde el xml
+        View v = inflador.inflate(R.layout.list_element, null);
+        v.setOnClickListener(onClickListener);
+        return new
+
+                ItemViewHolder(v);
+    }
+
+    // Usando como base el ViewHolder y lo personalizamos
+    @Override
+    public void onBindViewHolder(WearableRecyclerView.ViewHolder holder, int posicion) {
+        ItemViewHolder itemHolder = (ItemViewHolder) holder;
+        TextView view = itemHolder.textView;
+        view.setText(datos[posicion]);
+        holder.itemView.setTag(posicion);
+    }
+
+    // Indicamos el número de elementos de la lista
+    @Override
+    public int getItemCount() {
+        return datos.length;
+    }
+
+    public void setOnItemClickListener(View.OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+}
